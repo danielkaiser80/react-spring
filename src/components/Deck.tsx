3dffeec032b1f9353d6247e9711404f30d58e949
Daniel Kaiser <danielkaiser@fastmail.com>
2023-02-06 22:59:55 +0100

    Add Typescript, raise some more versions.

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,27 +1,24 @@
-import { render } from "react-dom"
 import React, { useState } from "react"
-import { useSprings, animated, interpolate } from "react-spring"
+import { animated, interpolate, useSprings } from "react-spring"
 import { useGesture } from "react-use-gesture"
-import "./assets/styles.css"
-
-import image0 from "./assets/IMG_4959.jpeg"
-import image1 from "./assets/IMG_5025.jpeg"
-import image2 from "./assets/IMG_5055.jpeg"
-import image3 from "./assets/IMG_5168.jpeg"
-import image4 from "./assets/IMG_5178.jpeg"
-import image5 from "./assets/IMG_5216.jpeg"
+import image0 from "../assets/IMG_4959.jpeg"
+import image1 from "../assets/IMG_5025.jpeg"
+import image2 from "../assets/IMG_5055.jpeg"
+import image3 from "../assets/IMG_5168.jpeg"
+import image4 from "../assets/IMG_5178.jpeg"
+import image5 from "../assets/IMG_5216.jpeg"
 
 const cards = [image0, image1, image2, image3, image4, image5]
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = (i) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
+const to = (i: number) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
 const from = () => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+const trans = (r: number, s: number) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
-function Deck() {
+const Deck = () => {
   const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-  const [props, set] = useSprings(cards.length, (i) => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
+  const [props, set] = useSprings(cards.length, (i) => ({ ...to(i), from: from() })) // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useGesture(({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
     const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
@@ -30,20 +27,29 @@ function Deck() {
     set((i) => {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flies out left or right, otherwise goes back to zero
       const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
       const scale = down ? 1.1 : 1 // Active cards lift up a bit
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
-    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set((i) => to(i)), 600)
+    if (!down && gone.size === cards.length) {
+      setTimeout(() => {
+        gone.clear()
+        setTimeout(() => set((i) => to(i)), 600)
+      }, 600)
+    }
   })
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-  return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
-      {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-      <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans), backgroundImage: `url(${cards[i]})` }} />
-    </animated.div>
-  ))
+  return (
+    <>
+      {props.map(({ x, y, rot, scale }, i) => (
+        <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
+          {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+          <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans), backgroundImage: `url(${cards[i]})` }} />
+        </animated.div>
+      ))}
+    </>
+  )
 }
 
-render(<Deck />, document.getElementById("root"))
+export default Deck
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom"
+import "./assets/styles.css"
+
+import Deck from "./components/Deck"
+import React from "react"
+
+render(<Deck />, document.getElementById("root"))
