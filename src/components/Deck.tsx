2450a8131438a121cd58fc0ac9b75c0dc3b8698b
Daniel Kaiser <danielkaiser@fastmail.com>
2023-02-06 23:31:16 +0100

    Upgrade the React version and the gesture package.

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -19,27 +19,29 @@ const Deck = () => {
   const [props, set] = useSprings(cards.length, (i) => ({ ...rotatedTo(i), from: { x: 0, rot: 0, scale: 1.5, y: -1000 } }))
 
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useGesture(({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
-    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+  const bind = useGesture({
+    onDrag: ({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
+      const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+      const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+      if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
 
-    set((i) => {
-      if (index !== i) return // We're only interested in changing spring-data for the current spring
+      set((i) => {
+        if (index !== i) return // We're only interested in changing spring-data for the current spring
 
-      const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flies out left or right, otherwise goes back to zero
-      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.1 : 1 // Active cards lift-up a bit
-      return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
-    })
+        const isGone = gone.has(index)
+        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flies out left or right, otherwise goes back to zero
+        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+        const scale = down ? 1.1 : 1 // Active cards lift-up a bit
+        return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
+      })
 
-    if (!down && gone.size === cards.length) {
-      setTimeout(() => {
-        gone.clear()
-        setTimeout(() => set((i) => rotatedTo(i)), 600)
-      }, 600)
-    }
+      if (!down && gone.size === cards.length) {
+        setTimeout(() => {
+          gone.clear()
+          setTimeout(() => set((i) => rotatedTo(i)), 600)
+        }, 600)
+      }
+    },
   })
 
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import React from "react"
 import { render } from "react-dom"
 import "./assets/styles.css"
 
 import Deck from "./components/Deck"
-import React from "react"
 
 render(<Deck />, document.getElementById("root"))
